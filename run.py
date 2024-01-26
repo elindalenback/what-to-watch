1d2025fdb7c25fd023185a43d263724aa1c25346
elindalenback <elin.dalenback@gmail.com>
2024-01-26 16:08:15 +0000

    Implement 'what_to_watch' function for user input and response.

--- run.py
+++ run.py
@@ -16,4 +16,28 @@ SHEET = GSPREAD_CLIENT.open('what_to_watch')
 movies = SHEET.worksheet('movies')
 
 data = movies.get_all_values()
-print(data)
+
+
+def what_to_watch():
+    """
+    Display a welcome message.
+    Prompt the user to choose between a movie, TV show, or both. 
+    Validate the input and provide a response. 
+    """
+    print("Welcome! What would you like to watch today?")
+    print("Please answer with 'movie', 'TV show' or 'both'\n")
+
+    what_to_watch_answer = input("Enter your answer here: ")
+    if what_to_watch_answer.lower() == 'both':
+        print("Excellent! You chose both.")
+    elif what_to_watch_answer.lower() == 'movie':
+        print("Excellent! You chose a movie.")
+    elif what_to_watch_answer.lower() == 'tv show' or what_to_watch_answer.lower() == 'tv-show':
+        print("Excellent! You chose a TV show.")
+    else:
+        print("Invalid input. Please enter 'movie', 'tv show', or 'both'.")
+
+def main():
+    what_to_watch()
+
+main()
