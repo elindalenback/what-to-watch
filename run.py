9cfb884c6ccb3db9f3d4b9659c8f02ff4c35a9aa
elindalenback <elin.dalenback@gmail.com>
2024-01-26 19:58:27 +0000

    Setting up basics for movie, TV show, and both selection

--- run.py
+++ run.py
@@ -23,6 +23,7 @@ def what_to_watch():
     Display a welcome message.
     Prompt the user to choose between a movie, TV show, or both. 
     Validate the input and provide a response. 
+    Loop the question for input until valid answer.
     """
     print("Welcome! What would you like to watch today?")
     print("Please answer with 'movie', 'TV show' or 'both'\n")
@@ -32,16 +33,28 @@ def what_to_watch():
         what_to_watch_answer = input("Enter your answer here: ").lower()
 
         if what_to_watch_answer in valid_responses:
-            print(f"Excellent! You chose {what_to_watch_answer}.")
+            print(f"Excellent! You chose {what_to_watch_answer}.\n")
         else:
-            print("Invalid input. Please enter 'movie', 'tv show', or 'both'.")
+            print("Invalid input. Please enter 'movie', 'tv show', or 'both'.\n")
+
+        # Decides wich function to run depending on user input 
+        if what_to_watch_answer == "movie":
+            movie_function()
+        elif what_to_watch_answer == "tv show" or what_to_watch_answer == "tv-show":
+            show_function()
+        elif what_to_watch_answer == "both":
+            both_function()
 
         if validation(what_to_watch_answer, valid_responses):
             break
 
-def validation(answer, responses):
+def validation(response, valid_responses):
+    """
+    Raises a ValueError if the input from the 
+    user is not a valid response.
+    """
     try:
-        if answer not in responses:
+        if response not in valid_responses:
             raise ValueError
 
     except ValueError as e:
@@ -49,6 +62,15 @@ def validation(answer, responses):
         return False
 
     return True
+
+def movie_function():
+    print("Hello from movie function")
+
+def show_function():
+    print("Hello from show function")
+
+def both_function():
+    print("Hello from both function")
 
 def main():
     what_to_watch()
